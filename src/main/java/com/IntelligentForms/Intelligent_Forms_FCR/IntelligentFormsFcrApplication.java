334d1eacb4ad43f70966a17256186cf110eaabc1
BeniCroitor <101263996+BeniCroitor@users.noreply.github.com>
2023-03-09 16:17:19 +0200

    Spring Iniatializr

---
+++ src/main/java/com/IntelligentForms/Intelligent_Forms_FCR/IntelligentFormsFcrApplication.java
@@ -0,0 +1,13 @@
+package com.IntelligentForms.Intelligent_Forms_FCR;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntelligentFormsFcrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntelligentFormsFcrApplication.class, args);
+	}
+
+}
