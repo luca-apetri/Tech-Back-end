0b6a8d151d2602d34c922ec27b8109a6a4f8e61d
Cosmin <cosmin.nita1@student.usv.ro>
2023-03-21 12:10:48 +0200

    Modificari

--- src/main/java/com/Intelligent_Forms/Intelligent_Forms_FCR/IntelligentFormsFcrApplication.java
+++ src/main/java/com/Intelligent_Forms/Intelligent_Forms_FCR/IntelligentFormsFcrApplication.java
@@ -3,15 +3,22 @@ package com.Intelligent_Forms.Intelligent_Forms_FCR;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @EnableScheduling
 @SpringBootApplication
+@Controller
+
 
 public class IntelligentFormsFcrApplication {
 
-	public static void main(String[] args) {
+    @RequestMapping("/")
+    @ResponseBody
+    public static void main(String[] args) {
 
-		SpringApplication.run(IntelligentFormsFcrApplication.class, args);
-	}
+        SpringApplication.run(IntelligentFormsFcrApplication.class, args);
+    }
 
 }
